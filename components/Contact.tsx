a0ad7e8c355572aad339b660d2583d001777abb8
Alamin Hossain <alaminhosssain97@gmail.com>
2023-06-18 13:44:04 +0600

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -7,10 +7,10 @@ const Contact = () => {
     className='max-w-contentContainer h-full mx-auto py-10 cl:py-32 flex flex-col gap-6 items-center justify-center'
     >
         <p className='font-codeFont text-lg text-textGreen font-semibold flex items-center tracking-wide'>
-            04. What's Next?
+           <p>04. What&apos;s Next?</p> 
         </p>
         <h2 className='font-titleFont text-2xl sm:text-3xl lgl:text-4xl font-semibold'>
-            Let's get in touch
+           <p>Let&apos;s get in touch</p>
         </h2>
         <p className='max-w-[600px] text-center text-textDark'>
             My inbox is always open and looking for new opportunities. <br /> Whether you have a question or just want to say hi, I'll try my best to get back to you!
