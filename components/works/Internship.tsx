f8957e86b0e43e8ce502436a6ccb6b37fad1c33d
Alamin Hossain <alaminhosssain97@gmail.com>
2023-06-18 15:12:22 +0600

    Update Internship.tsx

--- components/works/Internship.tsx
+++ components/works/Internship.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion';
-import { BsArrowRightShort } from "react-icons/Bs";
+import { BsArrowRightShort } from "react-icons/bs";
 
 
 const Internship = () => {
